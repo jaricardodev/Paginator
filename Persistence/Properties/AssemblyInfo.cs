22706d48d257919434373197c914ff0515e1a8b0
jose.hernandez <jose.hernandez@ascentis.com>
2021-07-30 19:29:25 -0300

    Added nuget config for Persistence and updated icon for model

---
+++ Persistence/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+
+
+[assembly: AssemblyTitle("Persistence")]
+[assembly: AssemblyCompany("Jaricardodev")]
+[assembly: AssemblyProduct("Paginator")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+
+[assembly: AssemblyVersion(ThisAssembly.Git.BaseVersion.Major + "." + ThisAssembly.Git.BaseVersion.Minor + "." + ThisAssembly.Git.BaseVersion.Patch)]
+
+[assembly: AssemblyFileVersion(ThisAssembly.Git.BaseVersion.Major + "." + ThisAssembly.Git.BaseVersion.Minor + "." + ThisAssembly.Git.BaseVersion.Patch)]
+
+[assembly: AssemblyInformationalVersion(
+    ThisAssembly.Git.BaseVersion.Major + "." +
+    ThisAssembly.Git.BaseVersion.Minor + "." +
+    ThisAssembly.Git.BaseVersion.Patch + "+" +
+    ThisAssembly.Git.Branch + "+" +
+    ThisAssembly.Git.Commit)]
